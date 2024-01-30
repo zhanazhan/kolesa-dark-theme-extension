17821db8322fa38d6fdc123fff457d34f874b0ea
Zhandos Ospanov <zhanazhan@gmail.com>
2024-01-30 18:39:51 +0600

    improvements

--- js/krisha.js
+++ js/krisha.js
@@ -1,1 +1,23 @@
-console.log('yeap');
+$(() => {
+    const selectors = [
+        '#place-vip',
+        '.ns-advert',
+        '.ddl_campaign'
+    ];
+    const maxTrials = 10;
+    let trialCounts = 0;
+    let interval = null;
+
+    const hideAd = () => {
+        if (maxTrials < trialCounts) {
+            clearInterval(interval);
+            return;
+        }
+        selectors.forEach((elem) => {
+            $(elem).hide();
+        });
+        trialCounts++;
+    };
+    hideAd();
+    interval = setInterval(hideAd, 2000);
+});
