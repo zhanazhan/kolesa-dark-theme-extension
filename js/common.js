4b8eca4d3f67f282119622908b12de21bae0eeec
Zhandos Ospanov <zhanazhan@gmail.com>
2024-02-01 19:14:03 +0600

    improvements

--- js/common.js
+++ js/common.js
@@ -15,24 +15,3 @@ const initHide = (selectors) => {
     };
     interval = setInterval(hideAd, 2000);
 };
-const initUnhide = (selectors) => {
-    var maxTrials2 = 1;
-    var trialCounts2 = 0;
-    var interval2 = null;
-    const unhide = () => {
-        if (maxTrials2 < trialCounts2) {
-            clearInterval(interval2);
-            return;
-        }
-        selectors.forEach((elem) => {
-            const $elem = $(elem);
-            const val = $elem.text().trim().replace('T', ' ').replace('+06:00', '');
-            $(elem).text(val).show();
-            const $elem2 = $(elem);
-            !$elem2.hasClass('reset-hot-time') && $elem2.addClass('reset-hot-time');
-        });
-        trialCounts2++;
-    };
-    unhide();
-    interval2 = setInterval(unhide, 2000);
-};
--- js/kolesa.js
+++ js/kolesa.js
@@ -3,8 +3,4 @@ $(() => {
         '.place-vip_newauto'
     ];
     initHide(selectors);
-    const selectorsUnhide = [
-        //'.hidden'
-    ];
-    initUnhide(selectorsUnhide);
 });
